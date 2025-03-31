47ae655fb520a49a896913a095b6922028cdb7aa
gitMiko0 <dustinbengo@gmail.com>
2025-03-30 21:05:19 -0600

    added tests

--- Tests.hs
+++ Tests.hs
@@ -6,6 +6,7 @@ import OutputWriter
 import System.Environment (getArgs)
 import Room
 import Group
+import Data.Time.Format (defaultTimeLocale, parseTimeM)
 
 -- Simplified Initialization
 group :: String -> String -> Int -> Bool -> Bool -> Bool -> Int -> String -> String -> Group
@@ -17,20 +18,28 @@ group start end size wheelchair projector computer floor dateStr groupId =
     in Group groupId size (dateTime date startTime) (dateTime date endTime) projector computer wheelchair floorPref
 
 
-
 room :: String -> Int -> Bool -> Bool -> Bool -> Int -> Room
 room roomId capacity wheelchair projector computer floor =
     Room roomId capacity wheelchair projector computer floor []
 
 -- Utility Functions
-parseDate :: String -> Day
-parseDate = read
-
-parseTimeOfDay :: String -> TimeOfDay
-parseTimeOfDay = read
+parseUTC :: String -> UTCTime
+parseUTC s = parseTimeOrError True defaultTimeLocale "%F %R" s
 
 dateTime :: Day -> TimeOfDay -> UTCTime
 dateTime date time = UTCTime date (timeOfDayToTime time)
+
+-- Safely parse a date string into Day
+parseDate :: String -> Day
+parseDate s = case parseTimeM True defaultTimeLocale "%F" s of
+    Just d -> d
+    Nothing -> error $ "Invalid date format: " ++ s
+
+-- Safely parse a time string into TimeOfDay
+parseTimeOfDay :: String -> TimeOfDay
+parseTimeOfDay s = case parseTimeM True defaultTimeLocale "%R" s of
+    Just t -> t
+    Nothing -> error $ "Invalid time format: " ++ s
 
 -- Unit Tests
 testCheckFloorPreference :: Test
@@ -65,11 +74,36 @@ testIsValidAssignment = TestList [
     "Invalid due to capacity" ~: isValidAssignment (group "10:00" "11:00" 15 False False False 1 "2023-01-01" "G1") (room "R1" 10 True True True 1) ~?= False
     ]
 
+testMissingEquip :: Test
+testMissingEquip = TestList [
+    "Valid assignment" ~: isValidAssignment (group "10:00" "11:00" 5 False False False 1 "2023-01-01" "G1") (room "R1" 10 True True True 1) ~?= True,
+    "Missing Equipments" ~: isValidAssignment (group "10:00" "11:00" 15 True True True 1 "2023-01-01" "G1") (room "R1" 10 False False False 1) ~?= False
+    ]
+
+testInvalidGroup :: Test
+testInvalidGroup = TestList [
+    -- Solution should be empty
+    "Invalid group" ~: assignRooms [(group "10:00" "11:00" 15 True True True 1 "2023-01-01" "G1")] [(room "R1" 10 False False False 1)] ~?= [] -- must wrap group and room in a list to match the type
+    ]
+
+testValidGroup :: Test
+testValidGroup = TestList [
+    let expectedStart = parseUTC "2023-01-01 10:00"
+        expectedEnd = parseUTC "2023-01-01 11:00"
+    in "Valid group" ~: assignRooms 
+        [group "10:00" "11:00" 5 True True True 1 "2023-01-01" "G1"]
+        [room "R1" 10 True True True 1]
+        ~?= [[("G1", "R1", expectedStart, expectedEnd)]]
+    ]
+
+
 main :: IO ()
 main = runTestTTAndExit $ TestList [
     testCheckFloorPreference,
     testCheckRoomCapacity,
     testCheckWheelchairAccess,
     testCheckEquipment,
-    testIsValidAssignment
+    testIsValidAssignment,
+    testInvalidGroup,
+    testValidGroup
     ]
